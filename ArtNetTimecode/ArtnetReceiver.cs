e05b145e974adbd0d2fdfc65d951d5c796ab7a53
Anthony Headley <1986602+hossimo@users.noreply.github.com>
2020-03-25 16:44:55 -0400

    Adding the start of the Receiver.
    Need to cleanup the way the Stub works.

--- ArtNetTimecode/ArtnetReceiver.cs
+++ ArtNetTimecode/ArtnetReceiver.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
 namespace ArtNetTimecode
 {
     public class ArtnetReceiver
     {
         static bool running;
+        const int ARTNET_PORT = 6454;
 
         public static void StopThread()
         {
@@ -13,10 +20,35 @@ namespace ArtNetTimecode
 
         public static void ThreadProc()
         {
-            Console.WriteLine("Starting");
+            UdpClient udpClient = new UdpClient(ARTNET_PORT);
+            IPEndPoint remote = new IPEndPoint(IPAddress.Any, ARTNET_PORT);
+
+            Console.WriteLine($"Listning on port {ARTNET_PORT}");
+
             running = true;
             while (running)
             {
+                Byte[] receivedBytes = udpClient.Receive(ref remote);
+                
+                // ignore my addresses
+                List<string> myAddresses = Tools.GetLocalIPAddress();
+                if (myAddresses.Contains(remote.Address.ToString()))
+                {
+                    continue;
+                }
+
+                GCHandle handle = GCHandle.Alloc(receivedBytes, GCHandleType.Pinned);
+                ArtNetTimecodePacket stubPacket;
+                try
+                {
+                    stubPacket = (ArtNetTimecodePacket)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(ArtNetTimecodePacket));
+                }
+                finally
+                {
+                    handle.Free();
+                }
+                Console.WriteLine($"Got 0x{stubPacket.opcode:X}");
+
                 Thread.Sleep(0);
             }
             Console.WriteLine("Exitting .");
---
+++ ArtNetTimecode/Tools.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace ArtNetTimecode
+{
+    class Tools
+    {
+        public static List<string> GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            List<string> result = new List<string>();
+            foreach (var item in host.AddressList)
+            {
+                result.Add(item.ToString());
+            }
+            return result;
+        }
+    }
+}
